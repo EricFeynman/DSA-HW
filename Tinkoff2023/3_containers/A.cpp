832aaf28349f41a2d49045a45a016cfaf6d495c6
Eric <147992178+EricFeynman@users.noreply.github.com>
2024-02-27 13:14:30 +0600

    Create A.cpp

---
+++ Tinkoff2023/3_containers/A.cpp
@@ -0,0 +1,37 @@
+/**
+ *  author: eric 
+ *  created: 02/26/24
+**/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    stack<int> a;
+    string s;
+    while (cin >> s) {
+        if (isdigit(s[0]) || (s.length() > 1 && s[0] == '-')) {
+            a.push(stoi(s));
+        } else {
+            int r = a.top();
+            a.pop();
+            int l = a.top();
+            a.pop();
+            switch (s[0]) {
+                case '+':
+                    a.push(l + r);
+                    break;
+                case '-':
+                    a.push(l - r);
+                    break;
+                case '*':
+                    a.push(l * r);
+                    break;
+            }
+        }
+    }
+    cout << a.top() << '\n';
+    return 0;
+}
